2c0808dfb352fdb50418abde2057f9e0d71e8aca
Siddharth <siddharthparashar2001@gmail.com>
2026-05-07 23:22:34 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const {ServerConfig, Logger} = require('./config');
 
 const apiRoutes = require("./routes")
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use("/api", apiRoutes)
 
 app.listen(ServerConfig.PORT, ()=>{
